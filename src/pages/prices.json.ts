5328521ea569a88300382536050468fb53012d36
Ian Baker <inbox@ianbaker.me>
2023-06-10 18:52:28 +0100

    Extracted prices to a separate endpoint

--- src/pages/prices.json.ts
+++ src/pages/prices.json.ts
@@ -1,10 +1,13 @@
-// Outputs: /builtwith.json
-export async function get({params, request}) {
+// Outputs: /prices.json
+export async function get() {
   return {
-    body: JSON.stringify({
-      size: 'Astro',
-      cost: 'https://astro.build/',
-      postage: 'a'
-    })
+    body: JSON.stringify([
+      {size: "Up to 250ml", cost: "16.00", postage: "1.00"},
+      {size: "251ml to 350ml", cost: "18.00", postage: "1.00"},
+      {size: "351ml to 660ml", cost: "23.00", postage: "1.00"},
+      {size: "661ml to 1000ml", cost: "26.00", postage: "1.00"},
+      {size: "Over 1000ml", cost: "35.00", postage: "3.00"},
+      {size: "Champagne (75cl)", cost: "27.00", postage: "3.00"}
+      ])
   };
 }
