fd6776a258c7bcca97a4b10dd3e04bc7e3f0cea0
Ian Baker <inbox@ianbaker.me>
2023-06-13 21:56:30 +0100

    Made prices SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://next.glassconstructions.co.uk",
+  output: "hybrid",
+  adapter: netlify()
 });
