84fc69f14c416f8d4bc0dcb09c4730d8e010e27d
Ian Baker <inbox@ianbaker.me>
2025-05-08 22:08:14 +0100

    Migrated to Tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,30 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 import icon from "astro-icon";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  trailingSlash: 'never',
+  trailingSlash: "never",
   integrations: [icon()],
+
   experimental: {
-    fonts: [{
-      provider: fontProviders.google(),
-      name: "Raleway",
-      cssVariable: "--font-body"
-    },
-    {
-      provider: fontProviders.google(),
-      name: "Architects Daughter",
-      cssVariable: "--font-headings"
-    }]
-  }
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Raleway",
+        cssVariable: "--font-body",
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Architects Daughter",
+        cssVariable: "--font-headings",
+      },
+    ],
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
